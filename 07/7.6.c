f83c6e60659c3f3a0b86e648ab5de92784a48b08
ycl <931415151@qq.com>
2023-07-27 23:19:36 +0800

    通过一份C/C++源代码验证程序的存储空间布局，txt+data+bss+堆+栈。 1.验证txt的大小，增删代码行数txt大小相应改变； 2.验证data的大小变化，初始化为非0的全局变量和静态变量相关，如果定义的变量需要分配的内存空间过大，则编译会变慢，甚至可能内存耗尽而导致编译失败； 3.验证bss的大小变化，未初始化或者初始化未0的全局变量和静态变量相关； 4.堆，负责动态分配的变量，分配过大的动态内存会导致程序运行失败； 5.栈，负责保存局部变量，以及函数调用和返回地址，临时变量过大或者调用函数过深都会导致栈溢出；

--- 07/7.6.c
+++ 07/7.6.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int maxcount[100];
+int maxcount[0x10000000];
 
 int main(int argc, char* argv[]) {
+    
+    //int tmp[0x10000000];
+    //int* ptr = (int*)malloc(sizeof(int) * 0x100000000);
+    //printf("%p\n", ptr);
     printf("hello world!\n");
+    maxcount[0] = 1;
+    //free(ptr);
     return 0;
 }
